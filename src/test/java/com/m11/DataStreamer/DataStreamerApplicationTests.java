ecf28c0821e58d251a9fc61235c79c3dea400e6e
mshemanskyi <mike.shemanskiy@gmail.com>
2021-04-04 20:43:33 +0300

    Initial commit

---
+++ src/test/java/com/m11/DataStreamer/DataStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m11.DataStreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
